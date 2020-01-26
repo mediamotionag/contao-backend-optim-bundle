aafdb46a9b80a151eea7c009f2519131ae1ee0a1
Rory Zünd <mail@rory.ch>
2020-01-26 10:42:46 +0100

    debuged the universele picker-label problem

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -12,8 +12,12 @@
 $objUser = \BackendUser::getInstance();
 if (isset($objUser) && $objUser->isAdmin) {
 	
-	# Show Article-ID
-	$GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'][] = 'id'; 
-	$GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span style="color: #fd9828; padding-left: 3px;">(<span style="color: #b7b7b7;">%s</span>, ID: %s)</span>';
+	# Only for article-listing
+	if(strpos($_SERVER['REQUEST_URI'],'table=') === false) {
 	
+		# Show Article-ID
+		$GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'] = array('title', 'inColumn', 'id');
+		$GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span style="color: #fd9828; padding-left: 3px;">(<span style="color: #b7b7b7;">%s</span>, ID: %s)</span>';
+		
+	}
 }
