75eded5033e3a4f7fad00855620bc88095f76fc5
Rory <zuend@mediamotion.ch>
2022-02-02 13:31:25 +0100

    TASK | Add better check (PHP 8) for admin

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -11,7 +11,7 @@
  
  # Show Module-ID
  $objUser = \BackendUser::getInstance();
-if (isset($objUser) && $objUser->isAdmin) {
+if (isset($objUser) && $objUser->admin == true) {
 	$GLOBALS['TL_DCA']['tl_module']['list']['sorting']['child_record_callback'] = array('tl_module_ids', 'listModule');
 }
 
