eb0ea8b237e0e88ee8f0d55b4871d92fc62fbbe1
Rory <zuend@mediamotion.ch>
2022-02-24 11:39:57 +0100

    Update tl_page.php

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -10,7 +10,7 @@
  */
  
 $objUser = \BackendUser::getInstance();
-if (isset($objUser) && $objUser->isAdmin) {
+if (BE_USER_LOGGED_IN && $objUser->isAdmin) {
 	
 	# Show Page-ID
 	$GLOBALS['TL_DCA']['tl_page']['list']['label']['fields'][] = 'id';
