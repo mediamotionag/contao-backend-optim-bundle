a2d482b35895746b3f43e3980796ee20b659ac1d
Rory <zuend@mediamotion.ch>
2022-06-16 10:40:12 +0200

    task | make compatible with contao 4.13 / php 8.1

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -8,13 +8,15 @@
  * @package   BackendOptimBundle
  * @author    Rory Zünd, Media Motion AG
  */
- 
- # Show Module-ID
- $objUser = \BackendUser::getInstance();
-if (BE_USER_LOGGED_IN && $objUser->isAdmin) {
-	$GLOBALS['TL_DCA']['tl_module']['list']['sorting']['child_record_callback'] = array('tl_module_ids', 'listModule');
-}
 
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Contao\System;
+use Symfony\Component\Security\Core\Security;
+
+$security = System::getContainer()->get('security.helper');
+if ($security->isGranted('ROLE_ADMIN')) {
+    $GLOBALS['TL_DCA']['tl_module']['list']['sorting']['child_record_callback'] = array('tl_module_ids', 'listModule');
+}
 
 /**
  * Class tl_module_ids
@@ -22,24 +24,23 @@ if (BE_USER_LOGGED_IN && $objUser->isAdmin) {
  */
 class tl_module_ids extends Backend {
 
-	
-	/**
-	* Import the back end user object
-	*/
-	public function __construct()
-	{
-		parent::__construct();
-	}
-	
-	
-	/**
-	* List a front end module
-	* @param array
-	* @return string
-	*/
-	public function listModule($row)
-	{
-		return '<div style="float:left;">'. $row['name'] .' <span style="color:#b7b7b7; padding-left:3px;">['.$GLOBALS['TL_LANG']['FMD'][$row['type']][0] .'] </span><span style="color:#fd9828;">(ID:' .$row['id']. ")</span></div>\n";
-	}
+    /**
+    * Import the back end user object
+    */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    
+    /**
+    * List a front end module
+    * @param array
+    * @return string
+    */
+    public function listModule($row)
+    {
+        return '<div style="float:left;">'. $row['name'] .' <span style="color:#b7b7b7; padding-left:3px;">['.$GLOBALS['TL_LANG']['FMD'][$row['type']][0] .'] </span><span style="color:#fd9828;">(ID:' .$row['id']. ")</span></div>\n";
+    }
 
 }
