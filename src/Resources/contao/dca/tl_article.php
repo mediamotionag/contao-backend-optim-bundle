a806e80713a1fff78fc70c62ffd545644c5a9c8d
Rory <zuend@mediamotion.ch>
2022-07-20 08:39:25 +0200

    task | fix logic error in frontend

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -9,21 +9,13 @@
  * @author    Rory Zünd, Media Motion AG
  */
 
-use Contao\CoreBundle\Security\ContaoCorePermissions;
-use Contao\System;
-use Symfony\Component\Security\Core\Security;
-
-if (System::getContainer()->get('contao.security.token_checker')->hasBackendUser()) {
-    $security = System::getContainer()->get('security.helper');
-    if ($security->isGranted('ROLE_ADMIN')) {
-
-        # Only for article-listing
-        if(strpos($_SERVER['REQUEST_URI'],'table=') === false) {
-
-            # Show Article-ID
-            $GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'] = array('title', 'inColumn', 'id');
-            $GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span style="color: #fd9828; padding-left: 3px;"><span style="color: #b7b7b7;">%s</span> (ID: %s)</span>';
+if (TL_MODE == 'BE') {
+    if($objUser = BackendUser::getInstance()){
+        if($objUser->admin == 1){
+            if(strpos($_SERVER['REQUEST_URI'],'table=') === false) {
+                $GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'] = array('title', 'inColumn', 'id');
+                $GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span style="color: #fd9828; padding-left: 3px;"><span style="color: #b7b7b7;">%s</span> (ID: %s)</span>';
+            }
         }
-
     }
 }
