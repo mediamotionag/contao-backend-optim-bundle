e1ddae3bd59e9d3952a9f0996177c326639205d6
Rory <zuend@mediamotion.ch>
2022-07-20 08:39:49 +0200

    task | fix logic error in frontend

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -9,14 +9,11 @@
  * @author    Rory Zünd, Media Motion AG
  */
 
-use Contao\CoreBundle\Security\ContaoCorePermissions;
-use Contao\System;
-use Symfony\Component\Security\Core\Security;
-
-if (System::getContainer()->get('contao.security.token_checker')->hasBackendUser()) {
-    $security = System::getContainer()->get('security.helper');
-    if ($security->isGranted('ROLE_ADMIN')) {
-        $GLOBALS['TL_DCA']['tl_module']['list']['sorting']['child_record_callback'] = array('tl_module_ids', 'listModule');
+if (TL_MODE == 'BE') {
+    if($objUser = BackendUser::getInstance()){
+        if($objUser->admin == 1){
+            $GLOBALS['TL_DCA']['tl_module']['list']['sorting']['child_record_callback'] = array('tl_module_ids', 'listModule');
+        }
     }
 }
 
@@ -27,19 +24,19 @@ if (System::getContainer()->get('contao.security.token_checker')->hasBackendUser
 class tl_module_ids extends Backend {
 
     /**
-     * Import the back end user object
-     */
+    * Import the back end user object
+    */
     public function __construct()
     {
         parent::__construct();
     }
-
-
+    
+    
     /**
-     * List a front end module
-     * @param array
-     * @return string
-     */
+    * List a front end module
+    * @param array
+    * @return string
+    */
     public function listModule($row)
     {
         return '<div style="float:left;">'. $row['name'] .' <span style="color:#b7b7b7; padding-left:3px;">['.$GLOBALS['TL_LANG']['FMD'][$row['type']][0] .'] </span><span style="color:#fd9828;">(ID:' .$row['id']. ")</span></div>\n";
