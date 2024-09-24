ed61d0d58ef2af6362ba874ff8671889b4ccd256
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-09-24 08:53:44 +0100

    check for Terminal42NodeBundle

--- src/Resources/contao/dca/tl_node.php
+++ src/Resources/contao/dca/tl_node.php
@@ -11,7 +11,8 @@
 
 use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
+use Terminal42\NodeBundle\Terminal42NodeBundle;
 
-if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+if (class_exists(Terminal42NodeBundle::class)) {
     $GLOBALS['TL_DCA']['tl_node']['list']['label']['label_callback'] = array('memo.backendoptim.listener.data_container', 'onCustomLabelCallback');
 }
